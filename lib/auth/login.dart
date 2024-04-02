82e955996f993a3ae7a2ef1370286e314f89905e
rohaan44 <rohaan.shaikh129@gmail.com>
2024-04-02 13:18:05 +0500

    My second commit

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -1,5 +1,6 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -14,13 +15,20 @@ class _LoginScreenState extends State<LoginScreen> {
 
 void Login(String email,Password) async{
 try {
-  final response = http.post(Uri.parse("https://reqres.in/api/register"),
+  final response = await http.post(Uri.parse("https://reqres.in/api/login"),
   body: {
-    email:
-  });
+    "email": email,
+    "password" : Password
 
-  
-} catch (e) {
+  });
+  if (response.statusCode==200) {
+    var data = jsonDecode(response.body.toString());
+    print(data['token']); 
+    print("Response Created");
+  }
+  else print("Response denied");
+} 
+catch (e) {
   print(e.toString());
 }
 
@@ -67,6 +75,7 @@ try {
             backgroundColor: Colors.blue
           ),
           onPressed: (){
+            Login(emailController.text, passwordController.text);
             
           }, child: Text("Login")))
         
